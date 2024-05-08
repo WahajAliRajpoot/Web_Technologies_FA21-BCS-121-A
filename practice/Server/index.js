d8ffbd2d7472588bcf9f0131ecc57d4ee9bd116b
WAHAJ ALI <121284672+WahajAliRajpoot@users.noreply.github.com>
2024-05-09 01:08:59 +0500

    EJS

--- practice/Server/index.js
+++ practice/Server/index.js
@@ -1,6 +1,17 @@
 const express = require('express')
+
 const  server = express()
-const PORT = 1120
-server.listen(PORT, ()=>{
-    console.log(`Server running on ${PORT}` )
+const PORT = 3000
+
+
+server.set("view engine", "ejs")
+// server.set("views","./abc")
+
+server.get("/", async (req,res)=>{
+    res.render("index")
 })
+
+server.listen(PORT, ()=>{
+    console.log(`Server running on ${PORT} \nhttp://localhost:${PORT}` )
+})
+
