c5c39c70577fb60e3226f1baaaadd142554b8841
WAHAJ ALI <121284672+WahajAliRajpoot@users.noreply.github.com>
2024-05-09 23:41:56 +0500

    created contct ejs and set paht for node js get method for contct methed

--- practice/Server/index.js
+++ practice/Server/index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-
 const  server = express()
 const PORT = 3500
 
@@ -10,7 +9,18 @@ server.use(express.static("public"))
 
 server.get("/", async (req,res)=>{
     res.render("index")
+  
 })
+
+server.get("/contact", async (req,res)=>{
+    res.render("contact")
+})
+server.get("/shop", async (req,res)=>{
+    res.render("shop")
+})
+
+
+
 
 server.listen(PORT, ()=>{
     console.log(`Server running on ${PORT} \nhttp://localhost:${PORT}` )
