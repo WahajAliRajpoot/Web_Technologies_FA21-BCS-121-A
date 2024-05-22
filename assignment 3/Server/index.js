9d7ed25b93de39195c6574e20112ef1a9b0f5fb9
WAHAJ ALI <121284672+WahajAliRajpoot@users.noreply.github.com>
2024-05-22 16:00:57 +0500

    assignment 3 converted landing page to express application

--- assignment 3/Server/index.js
+++ assignment 3/Server/index.js
@@ -1,71 +1,159 @@
-const express = require('express') //import express
-const  server = express()
-const PORT = 3500
-const ejsLayout = require("express-ejs-layouts")
-server.use(ejsLayout)
-//app.use acts as a middleware between request and response
-//you can also ceate your own middleware
-// cost mymiddleware = (req,res,next)=>{
-    //console.log(req)
-    //next()
-//}
-//server.use(mymiddleware)
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const ejsLayout = require("express-ejs-layouts");
 
-server.set("view engine", "ejs")
-// server.set("views","./abc")
+const server = express();
+const PORT = 3500;
 
-//app.use(express.static('public')) this isused to use a middleware and express.static is builtin middleware that
-//to make public folder public.Publlic folder contains public files that are accessible to anyone
-//middleware modifies your request
-server.use(express.static("public"))
+// MongoDB Connection URI
+const uri = 'mongodb://localhost:27017';
+let Products;
 
-//app.get or post or delete or app.get(path,handler),app.get("path",async(req,res)=>{})
-//get request means when you enter something on browser like any port is searched on google by me
-//nodemon is used such that whenever you make any change in code it restarts terminal
+// Connect to the MongoDB server
+const client = new MongoClient(uri);
+client.connect()
+  .then(() => {
+    console.log('Connected to MongoDB');
+    const db = client.db('your_database_name');
+    Products = db.collection('Products');
+  })
+  .catch(err => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+
+server.use(ejsLayout);
+server.set("view engine", "ejs");
+server.use(express.static("public"));
+
+// Routes
 server.get("/", async (req,res)=>{
-    res.render("index" )
-  
-})
+    res.render("index");
+});
 
 server.get("/contact", async (req,res)=>{
-    res.render("contact")
-})
+    res.render("contact");
+});
+
 server.get("/shop", async (req,res)=>{
-    res.render("shop")
-})
+    res.render("shop");
+});
+
 server.get("/about",async (req,res)=>{
-    res.render("about")
-})
+    res.render("about");
+});
+
 server.get("/login",async (req,res)=>{
-    res.render("login")
-})
+    res.render("login");
+});
+
 server.get("/signup",async (req,res)=>{
-    res.render("signup")
-})
+    res.render("signup");
+});
+
 server.post("/login",async (req,res)=>{
-    res.render("login")
-})
+    res.render("login");
+});
 
+// Add MongoDB-related route for listing products
+server.get("/products", async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = 10; // Number of products per page
+  const skip = (page - 1) * limit;
 
+  try {
+    const products = await Products.find().skip(skip).limit(limit).toArray();
+    res.render("products", { products });
+  } catch (err) {
+    console.error('Error fetching Products:', err);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
-
+// Start the server
 server.listen(PORT, ()=>{
-    console.log(`Server running on ${PORT} \nhttp://localhost:${PORT}` )
-})
+    console.log(`Server running on ${PORT} \nhttp://localhost:${PORT}` );
+});
 
-// server.get("/",":/name", async (req,res)=>{
-    // res.render("index" + req.params.name)
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express') //import express
+// const  server = express()
+// const PORT = 3500
+// const ejsLayout = require("express-ejs-layouts")
+// server.use(ejsLayout)
+// //app.use acts as a middleware between request and response
+// //you can also ceate your own middleware
+// // cost mymiddleware = (req,res,next)=>{
+//     //console.log(req)
+//     //next()
+// //}
+// //server.use(mymiddleware)
+
+// server.set("view engine", "ejs")
+// // server.set("views","./abc")
+
+// //app.use(express.static('public')) this isused to use a middleware and express.static is builtin middleware that
+// //to make public folder public.Publlic folder contains public files that are accessible to anyone
+// //middleware modifies your request
+// server.use(express.static("public"))
+
+// //app.get or post or delete or app.get(path,handler),app.get("path",async(req,res)=>{})
+// //get request means when you enter something on browser like any port is searched on google by me
+// //nodemon is used such that whenever you make any change in code it restarts terminal
+// server.get("/", async (req,res)=>{
+//     res.render("index" )
   
 // })
 
-//expresss and node js are almost same except some changes like you have to manually update anything in node js
-
-// server.get("/:slug", async (req,res)=>{
-//     console.log(req.params)
-// console.log(req.query
-    //res.render($(req.params.slug))
-//)
+// server.get("/contact", async (req,res)=>{
+//     res.render("contact")
+// })
+// server.get("/shop", async (req,res)=>{
+//     res.render("shop")
+// })
+// server.get("/about",async (req,res)=>{
+//     res.render("about")
+// })
+// server.get("/login",async (req,res)=>{
+//     res.render("login")
+// })
+// server.get("/signup",async (req,res)=>{
+//     res.render("signup")
+// })
+// server.post("/login",async (req,res)=>{
+//     res.render("login")
 // })
 
-//for post and put request harry created a function in script of html  for testing
+
+
+
+
+// server.listen(PORT, ()=>{
+//     console.log(`Server running on ${PORT} \nhttp://localhost:${PORT}` )
+// })
+
+// // server.get("/",":/name", async (req,res)=>{
+//     // res.render("index" + req.params.name)
+  
+// // })
+
+// //expresss and node js are almost same except some changes like you have to manually update anything in node js
+
+// // server.get("/:slug", async (req,res)=>{
+// //     console.log(req.params)
+// // console.log(req.query
+//     //res.render($(req.params.slug))
+// //)
+// // })
+
+// //for post and put request harry created a function in script of html  for testing
 
